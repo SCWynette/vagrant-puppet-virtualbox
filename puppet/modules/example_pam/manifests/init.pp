65bc1b6beade280d737ac13fb254bab40d04731c
Stephen Wynette <s.wynette@samsung.com>
2022-05-23 16:39:09 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:puppet/modules/example_pam/hiera.yaml

--- puppet/modules/example_pam/manifests/init.pp
+++ puppet/modules/example_pam/manifests/init.pp
@@ -1,26 +1,9 @@
 # example_pam class
 class example_pam (
-  $manage_nsswitch,
-  $pam_account_lines,
-  $pam_auth_lines,
-  $pam_password_auth_lines,
-  $pam_password_lines,
-  $pam_session_lines,
-  $pam_auth_files,
+  $options_hash,
 ) {
   class { '::pam':
-    manage_nsswitch         => $manage_nsswitch,
-    pam_account_lines       => $pam_account_lines,
-    pam_auth_lines          => $pam_auth_lines,
-    pam_password_auth_lines => $pam_password_auth_lines,
-    pam_password_lines      => $pam_password_lines,
-    pam_session_lines       => $pam_session_lines,
-    require                 => Class['::yp::bind'],
-  }
-
-  $pam_auth_files.each |$key,$value| {
-    file { $key:
-      * => $value,
-    }
+    *       => $options_hash,
+    require => Class['::yp::bind'],
   }
 }
--- puppet/site/profile/manifests/base/pam.pp
+++ puppet/site/profile/manifests/base/pam.pp
@@ -1,20 +1,4 @@
 # Example base pam profile
-class profile::base::pam (
-  $manage_nsswitch,
-  $pam_account_lines,
-  $pam_auth_lines,
-  $pam_password_auth_lines,
-  $pam_password_lines,
-  $pam_session_lines,
-  $pam_auth_files,
-) {
-  class { '::example_pam':
-    manage_nsswitch         => $manage_nsswitch,
-    pam_account_lines       => $pam_account_lines,
-    pam_auth_lines          => $pam_auth_lines,
-    pam_password_auth_lines => $pam_password_auth_lines,
-    pam_password_lines      => $pam_password_lines,
-    pam_session_lines       => $pam_session_lines,
-    pam_auth_files          => $pam_auth_files,
-  }
+class profile::base::pam {
+  include example_pam
 }
